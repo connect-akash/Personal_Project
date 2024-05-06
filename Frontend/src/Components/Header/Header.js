acb667ffa30cef3f34fdd9fb1bbe5dfbe087fdf0
akash <aka060786@gmail.com>
2024-05-06 16:21:22 +0530

    created header, body component

---
+++ Frontend/src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+
+import { useState } from "react";
+import './Header.css'
+
+function Header (){
+    
+
+    return(
+        <div className="header">
+                hello cruel world
+        </div>
+    )
+
+}
+
+export default Header
